7ef897d5dc32f01047b4b140c6444a01f04c928c
Owen Ceresa <co84635@mail.harpercollege.edu>
2024-09-20 16:22:17 -0500

    Addons added

--- src/main/java/com/csc/Factorizer.java
+++ src/main/java/com/csc/Factorizer.java
@@ -25,4 +25,55 @@ public class Factorizer {
 
     return primeFactors;
   }
+
+  public boolean isPrime(int n) {
+    if (n <= 1) {
+      return false;
+    }
+
+    int divisor = 2;
+    while (divisor * divisor <= n) {
+      if (n % divisor == 0) {
+        return false;
+      } else {
+        divisor++;
+      }
+    }
+    return true;
+  }
+
+  public boolean isComposite(int n) {
+    if (n <= 1) {
+      return false;
+    }
+
+    return !isPrime(n);
+  }
+
+  public String reduce(int numerator, int denominator) {
+    if (denominator == 0) {
+      return "Undefined";
+    }
+
+    if (numerator == 0) {
+      return "0";
+    }
+
+    int greatestCommonDivisor = 1;
+    for (int i = 1; i <= Math.min(numerator, denominator); i++){
+      if (numerator % i == 0 && denominator % i == 0) {
+        greatestCommonDivisor = i;
+      }
+    }
+
+    numerator /= greatestCommonDivisor;
+    denominator /= greatestCommonDivisor;
+
+    if (denominator == 1) {
+      return String.valueOf(numerator);
+    } else {
+      return numerator + "/" + denominator;
+    }
+  }
+
 }
--- src/test/java/com/csc/TestFactorizer.java
+++ src/test/java/com/csc/TestFactorizer.java
@@ -1,9 +1,9 @@
 package com.csc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestFactorizer {
 
@@ -56,5 +56,66 @@ public class TestFactorizer {
   public void testOneHundred() {
     assertEquals("2 2 5 5", Factorizer.printPrimeFactors(100));
   }
+
+  @Test
+  public void testIsPrime() {
+    assertEquals(true, factorizer.isPrime(3));
+  }
+
+  @Test
+  public void testIsPrime2() {
+    assertEquals(true, factorizer.isPrime(2));
+  }
+
+  @Test
+  public void testNotPrime() {
+    assertEquals(false, factorizer.isPrime(4));
+  }
+
+  @Test
+  public void testNotPrime2() {
+    assertEquals(false, factorizer.isPrime(6));
+  }
+
+  @Test
+  public void testReduceWholeNumber() {
+    assertEquals("3", factorizer.reduce(36,12));
+  }
+
+  @Test
+  public void testReduceWholeNumber2() {
+    assertEquals("1", factorizer.reduce(12,12));
+  }
+
+  @Test
+  public void testReduce() {assertEquals("6/7", factorizer.reduce(12,14));
+  }
+
+  @Test
+  public void testReduce2() {assertEquals("12/37", factorizer.reduce(12,37));
+  }
+
+  @Test
+  public void testisComposite(){
+      assertTrue(factorizer.isComposite(36));}
+
+  @Test
+  public void testisComposite2(){
+      assertTrue(factorizer.isComposite(12));}
+
+  @Test
+  public void testNotComposite(){
+      assertFalse(factorizer.isComposite(17));}
+
+  @Test
+  public void testNotComposite2(){
+      assertFalse(factorizer.isComposite(1));}
+
+  @Test
+  public void testNotComposite3(){
+    assertFalse(factorizer.isComposite(0));}
+
 }
+
+
 
